ae683bbf10dde0403fad1cc5e0fa7408741fb1f6
Michael <djibril536@gmail.com>
2024-02-05 14:32:25 +0200

    func advance tasks

---
+++ func_advance/task3.js
@@ -0,0 +1,33 @@
+const divide = (numerator, denominator) => {
+    if (denominator === 0) {
+        throw new Error("Error: Division by zero is undefined.");
+    } else if (typeof numerator !== 'number' || typeof denominator !== 'number'){
+        throw new Error("Both numerator and denominator must be numbers.");
+    } else {
+        return numerator / denominator;
+    }
+}
+
+try {
+    console.log(divide(10,2))
+} catch(e){
+    console.log('Error occurs', e)
+} finally {
+    console.log("Finish work")
+}
+
+try {
+    console.log(divide(10,0))
+} catch(e){
+    console.log('Error occurs', e)
+} finally {
+    console.log("Finish work")
+}
+
+try {
+    console.log(divide('Hello world',2))
+} catch(e){
+    console.log('Error occurs', e)
+} finally {
+    console.log("Finish work")
+}
