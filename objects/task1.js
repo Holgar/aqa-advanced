be8a0461594418fc6f11887cde6138055d30a59f
Michael <djibril536@gmail.com>
2024-02-12 15:18:57 +0200

    objects updated task1

--- objects/task1.js
+++ objects/task1.js
@@ -1,8 +1,8 @@
-const student = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 33,
-    courses: ['history', 'math', 'programming']
-};
+const student = {};
+
+student.firstName = 'John';
+student.lastName = 'Doe';
+student.age = 33;
+student.courses = ['history', 'math', 'programming']
 
 console.log(student);
